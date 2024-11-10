7dcd6282b89f7e2884128f72d09e5ec21c8873ee
James Cheng <jamescheng385@gmail.com>
2024-11-10 22:21:54 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from scipy.interpolate import interp1d
+
+# 已知数据点
+x = np.array([0, 1, 2, 3, 4, 5])
+y = np.array([0, 1, 4, 9, 16, 25])
+
+# 创建插值函数，'linear' 表示线性插值
+f = interp1d(x, y, kind='linear')
+
+# 生成新数据点
+x_new = np.linspace(0, 4, 100)
+y_new = f(x_new)
+
+# 绘制插值结果
+plt.plot(x, y, 'o', label='Known data')
+plt.plot(x_new, y_new, '-', label='Interpolated curve')
+plt.legend()
+plt.show()
